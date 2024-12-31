4b754e12ca3d7cba747028543f537ca6d6c81612
Chamalka Obadage <sadamalioc.21@uom.lk>
2024-12-31 18:36:42 +0530

    signup page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,19 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { SignUpComponent } from './Pages/sign-up/sign-up.component';
+import { EventListComponent } from './Components/profile/event-list/event-list.component';
+import { EventFormComponent } from './Components/event-form/event-form.component';
+import { DashboardComponent } from './Pages/dashboard/Dashboard.component';
+import { LandingComponent } from './Pages/landing/landing.component';
 
 export const routes: Routes = [
-
-
-    { path: 'login', component: DashboardComponent }, // Default route
-
-    { path: 'login', component: LoginComponent }, // Default route
-    { path: 'signup', component: SignUpComponent }, // Default route
+  { path: '', component: LandingComponent }, // Default route
+  { path: 'events', component: EventListComponent },
+  { path: 'event/new', component: EventFormComponent },
+  { path: 'event/edit/:id', component: EventFormComponent },
+  { path: 'login', component: LoginComponent }, // Default route
+  { path: 'signup', component: SignUpComponent }, // Default route
+  { path: 'dashboard', component: DashboardComponent }, // Default route
 
 
 ];
