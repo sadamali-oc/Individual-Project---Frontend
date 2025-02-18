d7860061d63ed62371c0aaa28070b32690ed6efb
Chamalka Obadage <sadamalioc.21@uom.lk>
2025-02-18 21:15:52 +0530

    add login function

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,8 @@ import { AdminComponent } from './Components/admin/admin.component';
 import { NewuserComponent } from './Components/newuser/newuser.component';
 import { NavigationBarComponent } from './Components/navigation-bar/navigation-bar.component';
 import { ReportComponent } from './Pages/report/report.component';
+import { OrganizerDashboardComponent } from './Pages/organizer-dashboard/organizer-dashboard.component';
+import { NormalUserDashboardComponent } from './Pages/normal-user-dashboard/normal-user-dashboard.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent }, // Default route
@@ -19,11 +21,14 @@ export const routes: Routes = [
   { path: 'event/edit/:id', component: EventFormComponent },
   { path: 'login', component: LoginComponent }, // Default route
   { path: 'signup', component: SignUpComponent }, // Default route
-  { path: 'dashboard', component: DashboardComponent }, // Default route
+  { path: 'admin/dashboard', component: DashboardComponent }, // Default route
   { path: 'users', component: UserComponent }, // Default route
   { path: 'organizer/create-event', component: CreateEventComponent },
   { path: 'admin-page', component: AdminComponent },
   { path: 'new-user-page', component: NewuserComponent  },
   {path:'navigation',component: NavigationBarComponent},
-  { path:'report', component:ReportComponent}
+  { path:'report', component:ReportComponent},
+  {path:'organizer/dashboard',component:OrganizerDashboardComponent},
+{path:'user/dashboard',component:NormalUserDashboardComponent}
+
 ];
